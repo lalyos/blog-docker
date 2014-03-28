7d348e6452d7e8b802a0a1ee240f388c2e619da7
lalyos <lalyos@yhoo.com>
2014-03-28 15:31:40 +0100

    wrapped script

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-[ -n "$DEBUG" ] && echo debug on ... && set -x && EXTRA_ENV=' -e "DEBUG=1" '
+if [ -n "$DEBUG" ];then
+  echo debug on ...
+  set -x
+  EXTRA_ENV=' -e "DEBUG=1" '
+fi
+
 : ${KEY_DIR:=~/.ssh}
+: ${VERBOSE:=1}
 
 is_valid_github_key() {
   ssh -a -T -o StrictHostKeyChecking=no -o IdentityFile=$1  -o IdentitiesOnly=yes git@github.com 2>&1 | grep "successfully authenticated" &>/dev/null
@@ -12,33 +18,27 @@ encode_file() {
   cat $1| base64 $WRAP_SWITCH 0
 }
 
-if [ -z "$GH_KEY" ]; then
-  echo searching for github key in $KEY_DIR
-  for key in $(find $KEY_DIR/ -type f -exec grep -l "BEGIN RSA PRIVATE KEY" {} \;) ; do
-    # checks if the key authenticates to github
+wrapper() {
+  if [ -z "$GH_KEY" ]; then
+    echo searching for github key in $KEY_DIR
+    for key in $(find $KEY_DIR/ -type f -exec grep -l "BEGIN RSA PRIVATE KEY" {} \;) ; do
+      if is_valid_github_key $key ; then
+        [ -n "$VERBOSE" ] && echo [VALID] $key
+        GH_KEY=$key
+        break
+      else
+        [ -n "$VERBOSE" ] && echo [INVALID] $key
+      fi
+    done
 
-    if is_valid_github_key $key; then
-      GH_KEY=$key
-      break
-    fi
-  done
-else
-  if is_valid_github_key $GH_KEY; then
-    echo valid GH_KEY=$GH_KEY
+    [ -z "$GH_KEY"  ] && echo [ERROR] no valid key in: KEY_DIR=$KEY_DIR && exit 1
   else
-    cat<<EOF
-
-  [ERROR] $GH_KEY isn't a valid github key
-    please check it with:
-    ssh -a -T -o StrictHostKeyChecking=no -o IdentityFile=$1  -o IdentitiesOnly=yes git@github.com
-EOF
-    exit 1
+    is_valid_github_key $GH_KEY || echo $GH_KEY is invalid fo github && exit 1
   fi
 
-fi
+  DOCKER_CMD="docker run -e KEY=$(encode_file $GH_KEY) $EXTRA_ENV -i sequenceiq/blog"
+  echo docker ...
+  # $DOCKER_CMD
+}
 
-: ${GH_KEY?'missing github private key please "export GH_KEY=<key_path>"'}
-
-
-DOCKER_CMD="docker run -e KEY=$(encode_file $GH_KEY) $EXTRA_ENV -i sequenceiq/blog"
-$DOCKER_CMD
+wrapper
