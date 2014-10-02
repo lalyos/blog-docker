8faa24073107920058cfbf586389b42d7135e274
lalyos <lalyos@yahoo.com>
2014-10-02 11:57:22 +0200

    deploy live script added

---
+++ deploy_live.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+: ${DEBUG:=1}
+: ${TRACE:=0}
+: ${RELEASE:=latest}
+: ${SOURCE_REPO:=sequenceiq/blog-test}
+: ${TARGET_REPO:=sequenceiq/blog}
+
+: ${TEMP_REL_DIR:=/tmp/release}
+
+[ $TRACE -gt 0 ] && set -x
+
+debug() {
+  [ $DEBUG -gt 0 ] && echo "[DEBUG] $*" 1>&2
+}
+
+get_latest_release_number() {
+  curl -s  https://api.github.com/repos/${SOURCE_REPO}/releases?per_page=1|jq .[0].tag_name -r
+}
+
+download_release() {
+  if [[ "$RELEASE" == "latest" ]]; then
+    RELEASE=$(get_latest_release_number)
+  fi
+  debug downloading tar RELEASE=$RELEASE
+
+  curl -L https://api.github.com/repos/${SOURCE_REPO}/tarball/${RELEASE} \
+    | tar xz
+}
+
+push_tar_to_gh_branch() {
+  git clone git@github.com:${TARGET_REPO}.git ${TEMP_REL_DIR}
+  cd ${TEMP_REL_DIR}
+
+  git checkout gh-pages
+  download_release
+
+  debug adding new files and modifications ...
+  git add .
+
+  # stage all file deletion (like: flat for sale)
+  git status -s|sed -n "s/^[ ]*D *//p"|xargs --no-run-if-empty git rm
+
+  git commit -m "Deploy release: $RELEASE"
+  git tag $RELEASE
+  git push origin gh-pages --tags
+  debug tag: $RELEASE pushed to github
+}
